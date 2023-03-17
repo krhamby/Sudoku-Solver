321f42f2ae88146eb404b5438f3fd55edaa1a57d
melvaloock <melvaloock@gmail.com>
2023-03-17 12:44:40 -0400

    complete test board

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -190,9 +190,10 @@ public class SudokuSolver {
     public static void main(String[] args) {
         // int[] testList = {1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7};
         // System.out.print(toStringTest(testList));
-        int[][] testBoard = {{1,2,3,6,7,8,9,4,5}, {5,8,4,2,3,9,7,6,1}, {9,6,7,1,4,5,3,2,8}, {4,5,6,7,8,9,1,2,3},
-         {5,6,7,8,9,1,2,3,4},{6,7,8,9,1,2,3,4,5},{7,8,9,1,2,3,4,5,6}, {8,9,1,2,3,4,5,6,7}, {}};
-        SudokuSolver test = new SudokuSolver();
+        int[][] completeBoard = {{1,2,3,6,7,8,9,4,5}, {5,8,4,2,3,9,7,6,1}, {9,6,7,1,4,5,3,2,8},
+         {3 ,7,2,4,6,1,5,8,9},{6,9,1,5,8,3,2,7,4},{4, 5,8,7,9,2,6,1,3}, {8,3,6,9,2,4,1,5,7}, {2,1,9,8,5,7,4,3,6},
+        {7, 4,5,3,1,6,8,9,2}};
+        SudokuSolver test = new SudokuSolver(completeBoard);
         System.out.print(test.toString());
     }
 }
